e4aed68ca99ac53de86fb5d8a9299ad3e65433c4
cmot17 <25753364+cmot17@users.noreply.github.com>
2023-08-11 23:43:08 -0400

    Rewrite association, add fuzzy-find

--- src/config.py
+++ src/config.py
@@ -1,37 +1,56 @@
 import os
 from datetime import timedelta
 from typing import Any, Literal, TypedDict
+from dataclasses import dataclass
 
-'----------------------------------------------------------------------------------'
-'                                     File Paths                                   '
-'----------------------------------------------------------------------------------'
+"----------------------------------------------------------------------------------"
+"                                     File Paths                                   "
+"----------------------------------------------------------------------------------"
 
-BASE_DIR = '/home/user/WLC'
+BASE_DIR = "/home/user/WLC"
 VRP_CLI_PATH = "/home/user/.cargo/bin/vrp-cli"
 
-AREA_ID = 'rosselli'
+AREA_ID = "rosselli"
 USE_COST_METRIC = False
 
-node_distance_table_file =      os.path.join(BASE_DIR, 'regions', AREA_ID, 'store', 'node_distances.json')
-house_distance_table_file =     os.path.join(BASE_DIR, 'regions', AREA_ID, 'store', 'house_distances.json')
-block_distance_matrix_file =    os.path.join(BASE_DIR, 'regions', AREA_ID, 'store', 'segment_distance_matrix.json')
-node_coords_file =              os.path.join(BASE_DIR, 'regions', AREA_ID, 'store', 'node_coords.json')
-block_output_file =             os.path.join(BASE_DIR, 'regions', AREA_ID, 'input', 'block_output.json')
-adjacency_list_file =           os.path.join(BASE_DIR, 'regions', AREA_ID, 'input', 'adjacency_list.json')
-coords_node_file =              os.path.join(BASE_DIR, 'regions', AREA_ID, 'input', 'coords_node.json')
-overpass_file =                 os.path.join(BASE_DIR, 'regions', AREA_ID, 'input', 'overpass.json')
+street_suffixes_file = os.path.join(
+    BASE_DIR, "src", "pre_processing", "street_suffixes.json"
+)
+
+node_distance_table_file = os.path.join(
+    BASE_DIR, "regions", AREA_ID, "store", "node_distances.json"
+)
+house_distance_table_file = os.path.join(
+    BASE_DIR, "regions", AREA_ID, "store", "house_distances.json"
+)
+block_distance_matrix_file = os.path.join(
+    BASE_DIR, "regions", AREA_ID, "store", "segment_distance_matrix.json"
+)
+node_coords_file = os.path.join(
+    BASE_DIR, "regions", AREA_ID, "store", "node_coords.json"
+)
+block_output_file = os.path.join(
+    BASE_DIR, "regions", AREA_ID, "input", "block_output.json"
+)
+adjacency_list_file = os.path.join(
+    BASE_DIR, "regions", AREA_ID, "input", "adjacency_list.json"
+)
+coords_node_file = os.path.join(
+    BASE_DIR, "regions", AREA_ID, "input", "coords_node.json"
+)
+overpass_file = os.path.join(BASE_DIR, "regions", AREA_ID, "input", "overpass.json")
 
 # Map addresses to block IDs
-houses_file =                   os.path.join(BASE_DIR, 'regions', AREA_ID, 'houses.json')
-blocks_file =                   os.path.join(BASE_DIR, 'regions', AREA_ID, 'blocks.json')
+houses_file = os.path.join(BASE_DIR, "regions", AREA_ID, "houses.json")
+blocks_file = os.path.join(BASE_DIR, "regions", AREA_ID, "blocks.json")
 
-address_pts_file =              os.path.join(BASE_DIR, 'input', 'address_pts.csv')
-problem_path =                  os.path.join(BASE_DIR, 'optimize', 'problem.json')
-solution_path =                 os.path.join(BASE_DIR, 'optimize', 'solution.json')
+address_pts_file = os.path.join(BASE_DIR, "input", "address_pts.csv")
+problem_path = os.path.join(BASE_DIR, "optimize", "problem.json")
+solution_path = os.path.join(BASE_DIR, "optimize", "solution.json")
 
-'----------------------------------------------------------------------------------'
-'                                     Constants                                    '
-'----------------------------------------------------------------------------------'
+"----------------------------------------------------------------------------------"
+"                                     Constants                                    "
+"----------------------------------------------------------------------------------"
 
 TURF_SPLIT = False  # Which problem to run
 
@@ -44,51 +63,52 @@ MINS_PER_HOUSE = 1.65
 CLUSTERING_CONNECTED_THRESHOLD = 100  # Meters where blocks are connected
 KEEP_APARTMENTS = False
 DISTANCE_TO_ROAD_MULTIPLIER = 0.5
-ALD_BUFFER = 5   # Meters after a block ends where a house is still on the block
+ALD_BUFFER = 5  # Meters after a block ends where a house is still on the block
 DIFFERENT_BLOCK_COST = 25
 
 
 # Cost of crossing the street (technically, in meters)
 DIFFERENT_SIDE_COST = {
-    'motorway': 400,
-    'trunk': 400,
-    'primary': 100,
-    'secondary': 60,
-    'tertiary': 35,
-    'unclassified': 20,
-    'residential': 20,
-    'service': 10
+    "motorway": 400,
+    "trunk": 400,
+    "primary": 100,
+    "secondary": 60,
+    "tertiary": 35,
+    "unclassified": 20,
+    "residential": 20,
+    "service": 10,
 }
 
 # Cost of crossing the street (technically, in meters)
 ROAD_WIDTH = {
-    'motorway': 20,
-    'trunk': 16,
-    'primary': 12,
-    'secondary': 10,
-    'tertiary': 8,
-    'unclassified': 8,
-    'residential': 6,
-    'service': 0
+    "motorway": 20,
+    "trunk": 16,
+    "primary": 12,
+    "secondary": 10,
+    "tertiary": 8,
+    "unclassified": 8,
+    "residential": 6,
+    "service": 0,
 }
-'----------------------------------------------------------------------------------'
-'                                       Type Hints                                 '
-'----------------------------------------------------------------------------------'
+"----------------------------------------------------------------------------------"
+"                                       Type Hints                                 "
+"----------------------------------------------------------------------------------"
 
 
 # NOTE: the 'type' and 'id' attributes are not required. When using Python 3.11,
 # wrap these attributes in the 'typing.NotRequired' hint to eliminate errors on instance creation.
 # On earlier versions, either suppress or ignore these errors: they do not affect json export or reading.
 
+
 class Point(TypedDict):
     lat: float
     lon: float
-    type: Literal['house', 'node', 'other']
+    type: Literal["house", "node", "other"]
     id: str
 
 
 def pt_id(p: Point) -> str:
-    '''
+    """
     Get the ID of a point
 
     Parameters:
@@ -96,9 +116,12 @@ def pt_id(p: Point) -> str:
 
     Returns:
         str: the ID, if it was provided upon creation. Otherwise, an ID made up of the rounded coordinates
-    '''
-    return str('{:.7f}'.format(p['lat'])) + ':' + str('{:.7f}'.format(p['lon'])) \
-        if 'id' not in p or p['id'] is None else p['id']
+    """
+    return (
+        str("{:.7f}".format(p["lat"])) + ":" + str("{:.7f}".format(p["lon"]))
+        if "id" not in p or p["id"] is None
+        else p["id"]
+    )
 
 
 house_t = dict[str, Point]
@@ -107,7 +130,7 @@ node_list_t = list[Point]
 # DEPOT = Point(lat=40.4471477, lon=-79.9311578, type='node')  # Kipling and Dunmoyle
 # DEPOT = Point(lat=40.4310603, lon=-79.9191268, type='node')  # Shady and Nicholson
 # DEPOT = Point(lat=40.4430899, lon=-79.9329246, type='node')  # Maynard and Bennington
-DEPOT = Point(lat=40.4362340, lon=-79.9191103, type='node')
+DEPOT = Point(lat=40.4362340, lon=-79.9191103, type="node")
 NUM_LISTS = 1
 
 
@@ -132,9 +155,9 @@ blocks_file_t = dict[str, Block]
 houses_file_t = dict[str, str]
 
 
-'----------------------------------------------------------------------------------'
-'                               Output File Type Hints                             '
-'----------------------------------------------------------------------------------'
+"----------------------------------------------------------------------------------"
+"                               Output File Type Hints                             "
+"----------------------------------------------------------------------------------"
 
 
 class Person(TypedDict):
@@ -149,9 +172,9 @@ class HousePeople(TypedDict):
     subsegment_start: int
 
 
-'----------------------------------------------------------------------------------'
-'                                 Solution Type Hints                              '
-'----------------------------------------------------------------------------------'
+"----------------------------------------------------------------------------------"
+"                                 Solution Type Hints                              "
+"----------------------------------------------------------------------------------"
 
 
 class Statistic(TypedDict):
@@ -191,9 +214,9 @@ class Solution(TypedDict):
     unassigned: list[Any]
 
 
-'----------------------------------------------------------------------------------'
-'                                  Problem Type Hints                              '
-'----------------------------------------------------------------------------------'
+"----------------------------------------------------------------------------------"
+"                                  Problem Type Hints                              "
+"----------------------------------------------------------------------------------"
 
 
 class Profile(TypedDict):
@@ -286,10 +309,12 @@ class DistanceMatrix(TypedDict):
     distances: list[int]
 
 
-'----------------------------------------------------------------------------------'
-'                             Optimization Parameters                              '
-'----------------------------------------------------------------------------------'
+"----------------------------------------------------------------------------------"
+"                             Optimization Parameters                              "
+"----------------------------------------------------------------------------------"
 OPTIM_COSTS = Costs(fixed=0, distance=3, time=1)
 
-OPTIM_OBJECTIVES = [[Objective(type='maximize-value')],
-                    [Objective(type='minimize-cost')]]
+OPTIM_OBJECTIVES = [
+    [Objective(type="maximize-value")],
+    [Objective(type="minimize-cost")],
+]
